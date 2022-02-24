3aacd4c91f78f492a35a2fa5d7db74015ffe1447
joshgit <jschermann91@gmail.com>
2022-02-24 16:44:28 -0600

    modal test added

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -4,7 +4,7 @@ import Modal from "../Modal";
 const PhotoList = ({ category }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentPhoto, setCurrentPhoto] = useState();
- 
+
   const [photos] = useState([
     {
       name: "Grocery aisle",
@@ -125,13 +125,15 @@ const PhotoList = ({ category }) => {
   const currentPhotos = photos.filter((photo) => photo.category === category);
 
   const toggleModal = (image, i) => {
-    setCurrentPhoto({...image, index: i})
-    setIsModalOpen(true)
-  }
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen && (
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
